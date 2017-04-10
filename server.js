fd6347326ae997784e55ca29115540925d7318af
Jeremy Foster <jeremy.foster@live.com>
2017-04-10 15:03:36 -0700

    make loaders recursive

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ let restify = require('restify');
 let _ = require('lodash');
 let fs = require('fs');
 let path = require('path');
+let readdir = require('readdir-enhanced');
 //envx confirms the existence of a environment variable and if it
 //doesnt exist it will show a friendly error message
 const envx = require("envx");
@@ -21,29 +22,26 @@ let bot = new builder.UniversalBot(connector, function (session) {
     //You can either set your preferred Locale like this (default is en if you dont do anything)
     session.preferredLocale("en");
     //if you want them to select their locale then call this
-/*    if (!session.userData['BotBuilder.Data.PreferredLocale']) {
-        session.beginDialog('/localePicker');
-    } */
+    /*    if (!session.userData['BotBuilder.Data.PreferredLocale']) {
+            session.beginDialog('/localePicker');
+        } */
     session.beginDialog('/default');
 });
 
 //events
-//Call getFileNames to 
 getFileNames('./app/events')
-    //Take list a strings (the base names) and pull in the files and node modules
-    .map(name => ({ name: name, fx: require(`./app/events/${name}`) }))
+    .map(file => Object.assign(file, { fx: require(file.path) }))
     //for each one, execute the function (what ever the module returns becomes function (fx))
     .forEach(event => event.fx(event.name, bot));
 
-// recognizers
+// // recognizers
 getFileNames('./app/recognizers')
-    .map(name => ({ recognizer: require(`./app/recognizers/${name}`) }))
-    .forEach(recognizer => bot.recognizer(recognizer.recognizer));
+    .map(file => Object.assign(file, { recognizer: require(file.path) }))
+    .forEach(r => bot.recognizer(r.recognizer));
 
-//dialogs
 getFileNames('./app/dialogs')
-    .map(name => ({ name: name, fx: require(`./app/dialogs/${name}`) }))
-    .forEach(dialog => dialog.fx(dialog.name, bot));
+    .map(file => Object.assign(file, { fx: require(file.path) }))
+    .forEach(dialog => dialog.fx || dialog.fx(dialog.name, bot));
 
 // middleware
 bot.use(builder.Middleware.dialogVersion({ version: 1.0, resetCommand: /^reset/i }));
@@ -54,7 +52,7 @@ bot.endConversationAction('goodbye', 'Goodbye :)', { matches: /^goodbye/i });
 //serve the bot
 let server = restify.createServer();
 server.post('/api/messages', connector.listen());
-server.listen(process.env.port || process.env.PORT || 3978,function () {
+server.listen(process.env.port || process.env.PORT || 3978, function () {
     console.log('%s listening to %s', server.name, server.url);
 });
 
@@ -64,10 +62,8 @@ server.listen(process.env.port || process.env.PORT || 3978,function () {
 //it is used the same way for recognizers, actions, etc.
 //filter by .js files 
 function getFileNames(dir) {
-    //get list of fils
-    return fs.readdirSync(dir)
-        //only files with .js
-        .filter(item => /.js$/.test(item))
-        //turn files into strings that just have the basename (not the .js)
-        .map(file => path.basename(file, '.js'))
+    return readdir.sync(dir, { deep: true })
+        .map(item => `.${path.posix.sep}${path.posix.join(dir, path.posix.format(path.parse(item)))}`) //normalize paths
+        .filter(item => !fs.statSync(item).isDirectory() && /.js$/.test(item)) //filter out directories
+        .map(file => ({ name: path.basename(file, '.js'), path: file }))
 }
