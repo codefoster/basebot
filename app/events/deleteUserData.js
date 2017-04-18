2b46c4e3cbc663181e262b3617366649df6ae7e9
Jeremy Foster <jeremy.foster@live.com>
2017-04-17 22:17:14 -0400

    fix deleteUserData event

--- app/events/deleteUserData.js
+++ app/events/deleteUserData.js
@@ -1,6 +1,7 @@
 module.exports = function (name, bot, ba) {
     bot.on(name, function (message) {
-        let session = bot.loadSession(message.address);
-        session.beginDialog("/delete");
+        bot.loadSession(message.address, function(err, session) {
+            session.beginDialog("/delete");
+        });
     })
 };
