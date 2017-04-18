eae0c5b458a8028bde79cbd8ac10e121ef142569
Jeremy Foster <jeremy.foster@live.com>
2017-04-18 07:15:04 -0300

    improve first run

--- app/dialogs/firstRunConversation.js
+++ app/dialogs/firstRunConversation.js
@@ -1,11 +1,16 @@
+//this dialog uses onFindAction and a "hasRunVersion" variable in conversationData
+//to be sure this dialog only runs once per conversation
+
 let melibotdb = require('../services/melibotdb');
+
+const MATCH = 1.1;
+const NOMATCH = 0.0;
 
 module.exports = function (name, bot, ba) {
     bot.dialog(`/${name}`, [
         function (session) {
-            //this should be the first time running this (for this conversation)
-            //so mark it as dirty
-            session.privateConversationData.dirty = true;
+            session.send("(first run for this conversation)");
+            session.privateConversationData.hasRun = true;
 
             //save the address in mongo
             melibotdb.saveAddress(session.message.address);
@@ -15,8 +20,7 @@ module.exports = function (name, bot, ba) {
     ])
         .triggerAction({
             onFindAction: function (context, callback) {
-                //only trigger this dialog the first time for this conversation
-                callback(null, (context.privateConversationData.dirty ? 0.0 : 1.1));
+            callback(null, (!context.privateConversationData.hasRun ? MATCH : NOMATCH));
             }
         })
 };
---
+++ app/dialogs/firstRunUser.js
@@ -0,0 +1,26 @@
+//this dialog uses onFindAction and a "hasRunVersion" variable in userData
+//to be sure this dialog only runs once per user
+//use "delete all" command to reset user data and test
+//increment currentVersion to cause this dialog to run again for all users
+
+let localeTools = require('../libraries/localeTools');
+
+const MATCH = 1.1;
+const NOMATCH = 0.0;
+const currentVersion = 1.0;
+
+module.exports = function (name, bot, ba) {
+    bot.dialog(`/${name}`, [
+        function (session, args, next) {
+            session.send("(first run for this user)");
+            session.userData.hasRunVersion = currentVersion;
+            localeTools.chooseLocale(session);
+        }
+    ]).triggerAction({
+        onFindAction: function (context, callback) {
+            let hasRunVersion = context.userData.hasRunVersion || 0;
+            callback(null, (hasRunVersion < currentVersion ? MATCH : NOMATCH));
+        }
+    })
+
+};
