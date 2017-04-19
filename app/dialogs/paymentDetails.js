af00ccb2700366d3ef042d11617f9892cb644b3a
Daniel Egan <daniel.egan@live.com>
2017-04-19 00:10:00 -0300

    added updateDescription stub with restify

--- app/dialogs/paymentDetails.js
+++ app/dialogs/paymentDetails.js
@@ -1,4 +1,9 @@
+let restify = require("restify");
+let builder = require('botbuilder');
+let utils = require('../helpers/utils');
 let user;
+let orders = require('../../samples/sampleBuyerOrders.json');
+
 
 module.exports = function (name, bot, ba) {
     bot.dialog(`/${name}`, ba.authenticate("mercadolibre").concat([
@@ -13,20 +18,30 @@ module.exports = function (name, bot, ba) {
             let mercadoLibreUrl = `orders/search?buyer=${user.id}&access_token=${user.accessToken}&order.status=payment_in_process`;
             client.get(mercadoLibreUrl, (err, req, res, obj) => {
                 if (!err) {
-                    console.log(obj);
-                    let msg = new builder.Message()
-                        .attachments([
-                            new builder.HeroCard(session)
-                                .text(user.first_name + ' ' + user.last_name)
-                            /*  .images([
-                                new builder.CardImage(session).url(obj.data.image['60x60'].url)
-                                ])*/
-                        ]
-                        );
-                    session.endDialog(msg);
+                   
+                    if (obj.results.length > 0) {
+
+                        session.send(`I found ${obj.results.length} orders, which one are you refering to?`)
+                        let msg = new builder.Message()
+                            .attachmentLayout(builder.AttachmentLayout.carousel)
+                            .attachments(obj.results.map(utils.productAsAttachment));
+
+                            ////If you wanted to send just one card you can do it this way
+                            // .attachments([
+                            //     new builder.HeroCard(session)
+                            //         .text(user.first_name + ' ' + user.last_name)
+                            //         .images([
+                            //             new builder.CardImage(session).url(obj.data.image['60x60'].url)
+                            //         ])
+                            // ]
+                            // );
+                        session.endDialog(msg);
+                    } else {
+                        console.log(err);
+                        session.endDialog("error getting profile, typing 'logout' to try again");
+                    }
                 } else {
-                    console.log(err);
-                    session.endDialog("error getting profile, typing 'logout' to try again");
+                    sesion.endDialog('we did not find any orders');
                 }
             });
         }
---
+++ app/dialogs/updateDescription.js
@@ -0,0 +1,29 @@
+let restify = require('restify');
+let builder = require('botbuilder');
+let user;
+
+module.exports = function (name, bot, ba) {
+    bot.dialog(`/${name}`, ba.authenticate("mercadolibre").concat([
+        function (session, args, next) {
+            //get the mercadolibre profile
+            user = ba.profile(session, "mercadolibre");
+            //call mercadolibre and get something using user.accessToken
+            let client = restify.createJsonClient({
+                url: 'https://api.mercadolibre.com',
+                accept: 'application/json',
+            });
+            let mercadoLibreUrl = `/items/${item_id}/description?access_token=${user.accessToken}`;
+            client.get(mercadoLibreUrl, (err, req, res, obj) => {
+                if (!err) {
+
+                    console.log('%d -> %j', res.statusCode, res.headers);
+                    console.log('%j', obj);
+                    session.endDialog('finished updating description');
+                } else {
+                    console.log(err);
+                    session.endDialog("error updating description");
+                }
+            });
+        }
+    ])).triggerAction({ matches: name });
+};
