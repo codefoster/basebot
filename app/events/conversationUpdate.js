7bb20076b207f551c7d382465273d0d91b4f5343
Jeremy Foster <jeremy.foster@live.com>
2017-04-22 12:33:06 -0400

    remove auth parameter from conversationUpdate event

--- app/events/conversationUpdate.js
+++ app/events/conversationUpdate.js
@@ -1,6 +1,6 @@
 let builder = require('botbuilder');
 
-module.exports = function (name, bot, auth) {
+module.exports = function (name, bot) {
 
     bot.on(name, function (message) {
         if (message.membersAdded) {
