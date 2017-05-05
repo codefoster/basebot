a8c8dbe256a5cb2e40355ca8145396d31317d1e7
Jeremy Foster <jeremy.foster@live.com>
2017-05-05 00:39:14 -0700

    refactor authenticationService slightly

--- app/services/authenticationService.js
+++ app/services/authenticationService.js
@@ -3,12 +3,20 @@ let authStrategy = require(`passport-${process.env.AUTH_PROVIDER_NAME}`).Strateg
 let session = require('express-session');
 
 let authenticator;
-let profile;
 
 let botAuthOptions = {
     baseUrl: `https://${process.env.WEBSITE_HOSTNAME}`,
     secret: process.env.BOTAUTH_SECRET
 };
+
+let factory = options => new authStrategy(
+    {
+        consumerKey: process.env.AUTH_PROVIDER_APP_ID,
+        consumerSecret: process.env.AUTH_PROVIDER_APP_SECRET,
+        callbackURL: options.callbackURL
+    },
+    strategyVerifyFunction
+);
 
 let strategyVerifyFunction = (accessToken, refreshToken, profile, done) => {
     profile = profile || {};
@@ -21,22 +29,13 @@ module.exports = {
     initialize: (server, bot) => {
         server.use(session({ secret: process.env.BOTAUTH_SECRET }));
         authenticator = new botauth.BotAuthenticator(server, bot, botAuthOptions)
-            .provider(
-            process.env.AUTH_PROVIDER_NAME,
-            options => new authStrategy({
-                consumerKey: process.env.AUTH_PROVIDER_APP_ID,
-                consumerSecret: process.env.AUTH_PROVIDER_APP_SECRET,
-                callbackURL: options.callbackURL
-            }, strategyVerifyFunction)
-            );
+            .provider(process.env.AUTH_PROVIDER_NAME, factory);
         return authenticator;
     },
-    isAuthenticated: (profile ? true : false),
-    profile: () => authenticator.profile(session, process.env.AUTH_PROVIDER_NAME),
     requireAuthentication: dialogFunctions => {
         //if already logged in, then just return the caller's dialogFunctions
-        if(profile) return dialogFunctions;
-        
+        if (session.privateConversationData && session.privateConversationData.user) return dialogFunctions;
+
         let authFunctions = authenticator.authenticate(process.env.AUTH_PROVIDER_NAME);
         authFunctions.push((session, args, next) => {
             session.privateConversationData.user = authenticator.profile(session, process.env.AUTH_PROVIDER_NAME);
@@ -45,5 +44,8 @@ module.exports = {
         let allFunctions = authFunctions.concat(dialogFunctions);
         return allFunctions;
     },
-    logout: session => authenticator.logout(session, process.env.AUTH_PROVIDER_NAME)
+    logout: session => {
+        delete session.privateConversationData.user;
+        authenticator.logout(session, process.env.AUTH_PROVIDER_NAME)
+    }
 };
