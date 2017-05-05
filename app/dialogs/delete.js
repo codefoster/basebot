6a3dc97394e4b064cb95eecbe82c089d7ecc44e9
Jeremy Foster <jeremy.foster@live.com>
2017-05-05 10:16:22 -0700

    add confirmation for userdata delete

--- app/dialogs/delete.js
+++ app/dialogs/delete.js
@@ -3,11 +3,15 @@
 let builder = require("botbuilder");
 
 module.exports = function (name, bot) {
-    bot.dialog(`/${name}`, session => {
-        session.userData = {}
-        session.endDialog('User data deleted')
-    }).triggerAction({
-        matches: name,
-        confirmPrompt: "This will wipe everything out. Are you sure?"
-    })
+    bot.dialog(`/${name}`, [
+        function (session, args, next) {
+            builder.Prompts.confirm(session, "confirm_data_delete");
+        },
+        function (session, args, next) {
+            if(args.response) {
+                session.userData = {};
+                session.endDialog('data_deleted');
+            }
+        }
+    ]).triggerAction({ matches: name })
 }
