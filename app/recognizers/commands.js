b9bc09a83dbb98865d849f45689f199e53da44a5
Jeremy Foster <jeremy.foster@live.com>
2017-05-05 10:19:36 -0700

    add help dialog

--- app/recognizers/commands.js
+++ app/recognizers/commands.js
@@ -1,6 +1,7 @@
 //if the command is typed to the bot, the corresponding intent
 //will be triggered by the recognizer
 let commands = [
+    { text: 'help', intent: 'help' },
     { text: 'sample', intent: 'sampleDialog' },
     { text: 'auth', intent: 'sampleAuthDialog' },
     { text: 'logout', intent: 'logout' },
@@ -10,6 +11,7 @@ let commands = [
 ];
 
 module.exports = {
+    commands: commands,
     recognize: function (context, callback) {
         let text = context.message.text.toLowerCase();
         let match = commands.find(m => m.text == text);
