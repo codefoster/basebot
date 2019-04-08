3159ca3be5b7a4731408114896a59b36dac90db2
Hattan Shobokshi <hattan@gmail.com>
2019-04-08 15:57:50 -0700

    added bot services

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,10 @@
 import { config } from 'dotenv';
 import * as path from 'path';
 import * as restify from 'restify';
+import { BotServices } from './botservices';
 
 // Import required bot services. See https://aka.ms/bot-services to learn more about the different parts of a bot.
-import { BotFrameworkAdapter, ConversationState, MemoryStorage, UserState } from 'botbuilder';
-
-// Import required bot configuration.
-import { BotConfiguration, IEndpointService } from 'botframework-config';
+import { BotFrameworkAdapter } from 'botbuilder';
 
 import { BasicBot } from './bot';
 
@@ -18,36 +16,14 @@ import { BasicBot } from './bot';
 const ENV_FILE = path.join(__dirname, '..', '.env');
 const loadFromEnv = config({ path: ENV_FILE });
 
-// Get the .bot file path.
-// See https://aka.ms/about-bot-file to learn more about .bot file its use and bot configuration.
-const BOT_FILE = path.join(__dirname, '..', (process.env.botFilePath || ''));
-let botConfig: BotConfiguration;
-try {
-    // Read bot configuration from .bot file.
-    botConfig = BotConfiguration.loadSync(BOT_FILE, process.env.botFileSecret);
-} catch (err) {
-    console.error(`\nError reading bot file. Please ensure you have valid botFilePath and botFileSecret set for your environment.`);
-    console.error(`\n - The botFileSecret is available under appsettings for your Azure Bot Service bot.`);
-    console.error(`\n - If you are running this bot locally, consider adding a .env file with botFilePath and botFileSecret.`);
-    console.error(`\n - See https://aka.ms/about-bot-file to learn more about .bot file its use and bot configuration.\n\n`);
-    process.exit();
-}
-
-// For local development configuration as defined in .bot file
-const DEV_ENVIRONMENT = 'development';
-
-// Define name of the endpoint configuration section from the .bot file.
-const BOT_CONFIGURATION = (process.env.NODE_ENV || DEV_ENVIRONMENT);
-
-// Get bot endpoint configuration by service name.
-// Bot configuration as defined in .bot file.
-const endpointConfig = botConfig.findServiceByNameOrId(BOT_CONFIGURATION) as IEndpointService;
+//Services Object to host all services used by the bot
+const botServices = new BotServices();
 
 // Create adapter.
 // See https://aka.ms/about-bot-adapter to learn more about .bot file its use and bot configuration .
 const adapter = new BotFrameworkAdapter({
-    appId: endpointConfig.appId || process.env.microsoftAppID,
-    appPassword: endpointConfig.appPassword || process.env.microsoftAppPassword,
+    appId: botServices.getEnvironmentVariable("MicrosoftAppID",false),
+    appPassword:  botServices.getEnvironmentVariable("MicrosoftAppPassword",false),
 });
 
 // Catch-all for errors.
@@ -59,37 +35,13 @@ adapter.onTurnError = async (context, error) => {
     // Send a message to the user
     context.sendActivity(`Oops. Something went wrong!`);
     // Clear out state
-    await conversationState.delete(context);
+    await botServices.conversationState.delete(context);
 };
 
-// Define a state store for your bot. See https://aka.ms/about-bot-state to learn more about using MemoryStorage.
-// A bot requires a state store to persist the dialog and user state between messages.
-let conversationState: ConversationState;
-let userState: UserState;
-
-// For local development, in-memory storage is used.
-// CAUTION: The Memory Storage used here is for local bot debugging only. When the bot
-// is restarted, anything stored in memory will be gone.
-const memoryStorage = new MemoryStorage();
-conversationState = new ConversationState(memoryStorage);
-userState = new UserState(memoryStorage);
-
-// CAUTION: You must ensure your product environment has the NODE_ENV set
-//          to use the Azure Blob storage or Azure Cosmos DB providers.
-// Add botbuilder-azure when using any Azure services.
-// import { BlobStorage } from 'botbuilder-azure';
-// // Get service configuration
-// const blobStorageConfig = botConfig.findServiceByNameOrId(STORAGE_CONFIGURATION_ID);
-// const blobStorage = new BlobStorage({
-//     containerName: (blobStorageConfig.container || DEFAULT_BOT_CONTAINER),
-//     storageAccountOrConnectionString: blobStorageConfig.connectionString,
-// });
-// conversationState = new ConversationState(blobStorage);
-// userState = new UserState(blobStorage);
-// Create the main dialog.
+// Create the bot instance.
 let bot;
 try {
-    bot = new BasicBot(conversationState, userState, botConfig);
+    bot = new BasicBot(botServices);
 } catch (err) {
     console.error(`[botInitializationError]: ${err}`);
     process.exit();
